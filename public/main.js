d99af91d0f4cc0c80f9d67439064c48e30cb0ff2
Thomas Wolfe <tewolfe2@gmail.com>
2016-06-22 19:27:20 -0700

    front end done

--- public/main.js
+++ public/main.js
@@ -1,25 +1,52 @@
 'use strict';
 
-let app = angular.module('myApp', []);
+let app = angular.module('myApp', ['ngStorage']);
 
 
-app.controller('mainCtrl', function($scope, $interval) {
+app.controller('mainCtrl', function($scope, $interval,$localStorage, $sessionStorage) {
 
+  $scope.total = 0;
+  $scope.propertyName=null;
+  $scope.reverse = true;
+  // $scope.$storage = $localStorage;
 
-  $scope.contacts = [{name:'thomas', email: 'test@emal.com', phone:'32178912'}];
+  $scope.sortBy = (propertyName) => {
+    $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+    $scope.propertyName = propertyName;
+  }
 
-  $scope.addContact = () => {
+  $scope.trans = [{description:'buy car', date:'2018-05-31T07:00:00.000Z', amount: -5000, type:'debit'}];
 
-    let newContact = angular.copy($scope.newContact);
-    $scope.contacts.push(newContact);
-    $scope.newContact = '';
+  $scope.addTrans = () => {
+
+    if($scope.newTrans.type === 'debit') {
+      $scope.newTrans.type = -$scope.newTrans.type;
+    }    
+    $scope.trans.push($scope.newTrans);
+    $scope.newTrans = '';
 
 
   };
 
-  $scope.deleteContact = ($index) => {
-    $scope.contacts.splice($index,1);
+  $scope.deleteTrans = ($index) => {
+    $scope.trans.splice($index,1);
 
-  }
+  };
+
+
+
+  $scope.getTotal = () => {
+    let total = 0;
+    for(let i = 0; i < $scope.trans.length; i++) {
+      total += $scope.trans[i].amount;
+    }
+
+    $scope.total= total;
+    // console.log($scope.total);
+    return total;
+
+  };
 
 });
+
+
